7132e709650a980521f9362189fd5e6987d36925
Harold <hhausman@gmail.com>
2009-07-04 15:18:03 -0600

    Some error checking on choices.

--- clj-wars.clj
+++ clj-wars.clj
@@ -1,11 +1,11 @@
 (ns clj-wars
   (:use [clojure.contrib server-socket duck-streams]))
 
-(defn item-to-graph-str [in-index in-item] 
+(defn item-to-graph-str [in-index in-item]
   (apply str (map #(format "%d -> %s;\n" in-index %) in-item)))
-(defn seq-to-graph-str [in-seq] 
+(defn seq-to-graph-str [in-seq]
   (apply str (map item-to-graph-str (iterate inc 0) in-seq)))
-(defn get-graph-str [in-seq] 
+(defn get-graph-str [in-seq]
   (apply str ["digraph G {\n" (seq-to-graph-str in-seq) "}\n"]))
 (defn write-graph-file [in-seq]
   (doto (new java.io.FileWriter "a.dot") (.write (get-graph-str in-seq)) (.close)))
@@ -26,17 +26,27 @@
   (print "\r\n")
   (flush))
 
-(defn print-menu [player]
+(defn get-menu [player]
+  (let [menu (ref [])]
+    (dosync
+     (doseq [option (sort (nth @*sectors* (:in-sector @player)))]
+       (alter menu conj [option (apply str "Go to sector " option ".")])))
+    menu))
+
+(defn print-menu [player menu]
   (write "Hello, " (:name @player) ".")
   (write "You're in sector " (:in-sector @player) ".")
-  (doseq [exit (sort (nth @*sectors* (:in-sector @player)))]
-    (write "(" exit ") - Go to sector " exit "."))
+  (doseq [option @menu]
+    (let [choice (first option)
+          text (first (rest option))]
+      (write "(" choice ") - " text)))
   (print "> ")
   (flush))
 
-(defn execute-choice [choice player]
-  (write "Going to sector " choice ".")
-  (dosync (alter player assoc :in-sector (Integer. choice))))
+(defn execute-choice [choice player menu]
+  (if (some #{(str choice)} (map #(str (first %)) @menu))
+    (dosync (alter player assoc :in-sector (Integer. choice)))
+    (write "Invailid choice.")))
 
 (defn client-handler [in out]
   (binding [*in* (reader in)
@@ -44,13 +54,13 @@
     (write "Enter your name:")
     (print "> ")(flush)
     (let [player (ref {:name (read-line)
-                       :in-sector (rand-int 30)})]
-      (loop []
-        (print-menu player)
-        (execute-choice (read-line) player)
+                       :in-sector (rand-int *sector-count*)})]
+      (loop [menu (get-menu player)]
+        (print-menu player menu)
+        (execute-choice (read-line) player menu)
         (flush)
-        (recur)))))
-  
+        (recur (get-menu player))))))
+
 (def port 8866)
 
 ;; This noise makes recompiling in the repl restart the server
