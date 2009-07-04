5548eaee7f553dfb704b457a75189a0eaf30f7bd
Harold <hhausman@gmail.com>
2009-07-03 18:54:59 -0600

    Forgot these.

--- clj-wars.clj
+++ clj-wars.clj
@@ -1,6 +1,19 @@
 (ns clj-wars
   (:use [clojure.contrib server-socket duck-streams]))
 
+(defn item-to-graph-str [in-index in-item] 
+  (apply str (map #(format "%d -> %s;\n" in-index %) in-item)))
+(defn seq-to-graph-str [in-seq] 
+  (apply str (map item-to-graph-str (iterate inc 0) in-seq)))
+(defn get-graph-str [in-seq] 
+  (apply str ["digraph G {\n" (seq-to-graph-str in-seq) "}\n"]))
+(defn write-graph-file [in-seq]
+  (doto (new java.io.FileWriter "a.dot") (.write (get-graph-str in-seq)) (.close)))
+(defn write-graph-image [in-seq]
+  (let [process (new java.lang.ProcessBuilder ["dot" "-Tpng" "a.dot" "-o" "a.png"])]
+    (write-graph-file in-seq)
+    (.start process)))
+
 (def *sector-count* 25)
 (defn get-rands [index] (distinct (take 4 (repeatedly #(rand-int *sector-count*)))))
 (defn create-sectors [] (map get-rands (range *sector-count*)))
